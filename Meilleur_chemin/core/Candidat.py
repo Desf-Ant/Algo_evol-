d3815047d4b8d49f79a4d73eb50ebe6cd7e5f6ac
ThePetman <79446372+ThePetman@users.noreply.github.com>
2022-12-02 16:43:09 +0100

    fin de journee

--- Meilleur_chemin/core/Candidat.py
+++ Meilleur_chemin/core/Candidat.py
@@ -2,7 +2,9 @@
 
 class Candidat: 
 
-  
+    odd_to_mutate = [0.5]*100
+
     def __init__(self,liste) :
         self.attributs = liste
+        #print(self.attributs)
         score = -1000
--- Meilleur_chemin/core/Evolve.py
+++ Meilleur_chemin/core/Evolve.py
@@ -1,24 +1,44 @@
-from random import random
+from random import random, shuffle
 from core.Graphe import *
 from core.Candidat import *
 
-def __init__ (self) :
+class Evolve :
+
+    def __init__ (self,graphe) :
         self.liste_solutions = []
         self.liste_next_generation = []
         self.nb_solutions = 100
-        #self.run()
-        self.graphe = Graphe()
-
-
-def initialize(self):
-        liste_base =[]
+        self.graphe = graphe
+        self.score_min = 1
+        self.best_solution = None
+        self.liste_base =[]
         for i in range(100):
-            liste_base.append(i)
+            self.liste_base.append(i)
+        self.run()
+        
+
+
+    def run(self) : 
+        self.initialize()
+        index_score, score = self.anotherGeneration()
+        while score > self.score_min :
+            self.calcul_score()
+            self.selectionAndCroisement()
+            self.mutation()
+            self.goToNextGeneration()
+        print(self.liste_solutions)
+        self.best_solution = self.liste_solutions[index_score]
+
+    def initialize(self):
+        
+
+       
         for _ in range(self.nb_solutions) :
-            self.liste_solutions.append(Candidat(shuffle(liste_base))  )
+            shuffle(self.liste_base)
+            self.liste_solutions.append(Candidat(self.liste_base)  )
 
 
-def calcul_score (self) :
+    def calcul_score (self) :
         for sol in self.liste_solutions :
             fit = self.fitness_function(sol)
             pen = self.penality_function(sol)
@@ -26,18 +46,18 @@ def calcul_score (self) :
             sol.score = fit + pen
 
 
-def fitness_function (self, solution) :
+    def fitness_function (self, solution) :
         somme = 0
-        for i in range(1,len(solution)) : 
-            somme += self.graphe.matrice_distance[solution[i]][solution[i-1]]
+        for i in range(1,len(self.liste_solutions)) : 
+            somme += self.graphe.matrice_distances[solution.attributs[i]][solution.attributs[i-1]]
         return somme 
     
-def penality_function(self, solution) :
+    def penality_function(self, solution) :
         penality = 0
         test = 0
-        for i in range(len(solution)) : 
+        for i in range(len(solution.attributs)) : 
             for j in range(100):
-                if solution[i] == j :
+                if solution.attributs[i] == j :
                     test += 1
         if test != 100:
             penality = 2000
@@ -45,16 +65,15 @@ def penality_function(self, solution) :
 
 
 
-def selectionAndCroisement(self) :
-        for i in range(self.nb_solutions/2):
+    def selectionAndCroisement(self) :
+        for i in range(int(self.nb_solutions/2)):
             indexS1, indexS2 = self.selection()
             self.croisement(indexS1, indexS2)
             
         if len(self.liste_next_generation) != self.nb_solutions :
             print("Moins de solution dans la nouvelle génération")
 
-        self.liste_solutions = self.liste_next_generation
-        self.liste_next_generation = []
+        
 
 
 
@@ -63,15 +82,15 @@ def selectionAndCroisement(self) :
 
 
 
-def selection(self) :
-        s1, s2 = randint(0,self.nb_solution), randint(0,self.nb_solutions)
+    def selection(self) :
+        s1, s2 = randint(0,self.nb_solutions-1), randint(0,self.nb_solutions-1)
         return s1, s2
 
 
-def croisement(self,indexS1, indexS2) :
-        pivot = randint(0, 100)
-        attributs_1 = self.liste_solutions[indexS1][:pivot] + self.liste_solutions[indexS2][pivot:]
-        attributs_2 = self.liste_solutions[indexS2][:pivot] + self.liste_solutions[indexS1][pivot:]
+    def croisement(self,indexS1, indexS2) :
+        pivot = randint(1, 99)
+        attributs_1 = self.liste_solutions[indexS1].attributs[:pivot] + self.liste_solutions[indexS2].attributs[pivot:]
+        attributs_2 = self.liste_solutions[indexS2].attributs[:pivot] + self.liste_solutions[indexS1].attributs[pivot:]
 
         new_sol_1 = Candidat(attributs_1)
         new_sol_2 = Candidat(attributs_2)
@@ -79,3 +98,25 @@ def croisement(self,indexS1, indexS2) :
 
         self.liste_next_generation.append(new_sol_1)
         self.liste_next_generation.append(new_sol_2)
+
+
+    def mutation(self) :
+        for i in range(len(self.liste_next_generation)) :
+            for j in range(len(self.liste_next_generation[i].attributs)) :
+                if random() <= Candidat.odd_to_mutate[j] :
+                    self.liste_next_generation[i].attributs[j] = randint(0,100)
+    
+    def goToNextGeneration(self):
+        self.liste_solutions = self.liste_next_generation
+        self.liste_next_generation = []
+
+    def anotherGeneration(self):
+        scores = []
+        for sol in self.liste_solutions: 
+            scores.append(self.fitness_function(sol))
+        return scores.index(min(scores)), min(scores)
+
+
+if __name__ == "__main__" :
+    d = Evolve()
+    print("La meilleure solution est", d.best_solution)
